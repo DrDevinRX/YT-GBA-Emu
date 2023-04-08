52bf641103e80af10fe56e3266a33a1c21b3db2d
DrDevinRX <22141991+DrDevinRX@users.noreply.github.com>
2023-04-08 00:19:37 -0400

    Does nothing yet

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
-﻿namespace GBA
+﻿using YouTube.Base;
+using YouTube.Base.Clients;
+
+namespace GBA
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<OAuthClientScopeEnum> scopes = new()
+            {
+                OAuthClientScopeEnum.ReadOnlyAccount
+            };
+
+            string? clientId = System.Environment.GetEnvironmentVariable("YT_CID");
+            string? clientSecret = System.Environment.GetEnvironmentVariable("YT_CS");
+
+            if (clientId is null && clientSecret is null)
+            {
+                Console.WriteLine("Bad client id or client secret");
+                Environment.Exit(1);
+            }
+
+            YouTubeConnection youTubeConnection = await YouTubeConnection.ConnectViaLocalhostOAuthBrowser(clientId, clientSecret, scopes);
+
+            Console.WriteLine(youTubeConnection.ToString());
         }
     }
 }
